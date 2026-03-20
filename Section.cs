734cd3b934fa899f213fb14862134a0340accf0f
EdPortugal05 <Darkchemodc@gmail.com>
2026-03-20 21:08:54 +0800

    Create Section.cs

---
+++ Section.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Examination.Models;
+
+public class Section
+{
+    public int SectionId { get; set; }
+
+    [Required]
+    public string SectionCode { get; set; }
+
+    public int CourseId { get; set; }
+    public int InstructorId { get; set; }
+
+    [Required]
+    public string Room { get; set; }
+
+    [Required]
+    public string Schedule { get; set; }
+}
