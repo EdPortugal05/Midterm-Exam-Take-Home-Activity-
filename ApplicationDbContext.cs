ad46576bc822d30489b622c2d3926bbd5300c42e
EdPortugal05 <Darkchemodc@gmail.com>
2026-03-20 21:03:17 +0800

    Create ApplicationDbContext.cs

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Midterm_Examination;
+using Midterm_Examination.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<Instructor> Instructors { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Section> Sections { get; set; }
+}
