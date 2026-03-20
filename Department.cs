f7e6b5d6216ee2bc154a2593be29060c0862f87a
EdPortugal05 <Darkchemodc@gmail.com>
2026-03-20 21:07:33 +0800

    Create Department.cs

---
+++ Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Examination.Models;
+
+public class Department
+{
+    public int DepartmentId { get; set; }
+
+    [Required]
+    public string Code { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Office { get; set; }
+}
