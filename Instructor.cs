17638b3e2d9d82c22c6073dcc2ff7c6308ac064d
EdPortugal05 <Darkchemodc@gmail.com>
2026-03-20 21:08:26 +0800

    Create Instructor.cs

---
+++ Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Examination.Models;
+
+public class Instructor
+{
+    public int InstructorId { get; set; }
+
+    [Required]
+    public string EmployeeNo { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public int DepartmentId { get; set; }
+}
