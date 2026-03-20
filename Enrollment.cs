54976064a26b6c0b75ef6e3cac5c60d34f82712e
EdPortugal05 <Darkchemodc@gmail.com>
2026-03-20 21:08:03 +0800

    Create Enrollment.cs

---
+++ Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Examination.Models;
+
+public class Enrollment
+{
+    public int EnrollmentId { get; set; }
+
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+
+    [Required]
+    public string Semester { get; set; }
+
+    [Range(1.0, 5.0)]
+    public double Grade { get; set; }
+}
